6eead1aed179e5281d274eb4cb107bd56174c60a
admin <admin@gmail.com>
2024-07-11 12:40:50 +0500

    HTML CSS JAVASCRIPT LOADER

---
+++ First/index.js
@@ -0,0 +1,19 @@
+$(document).ready(function () {
+    var $scramble = $(".scramble");
+    $scramble.scramble(3000, 20, "alphabet", true);
+})
+
+var t1 = gsap.timeline();
+t1.to(".Loading", {
+    opacity : 0,
+    delay: 2.5,
+})
+.to(".loader", {
+    opacity : 0,
+    duration: 1,
+    ease: "Expo.easeInOut"
+})
+.to(".loader", {
+    y: "-100%",
+    duration: 1,
+})
